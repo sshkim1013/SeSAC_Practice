9789146c7c95725548b9be7eba31ffd77ddc4d5c
sshkim1013 <sshkim1013@naver.com>
2025-10-21 15:53:58 +0900

    Delete chat-bot-app

--- chat-bot-app/src/pages/CreateMemoPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function CreateMemoPage() {
-  return (
-    <div>CreateMemoPage</div>
-  )
-}
--- chat-bot-app/src/utils/genai.js
+++
@@ -1,8 +0,0 @@
-// 기본 컨텐츠 생성형 AI
-import { GoogleGenAI } from "@google/genai";
-// 환경 변수 GEMINI API KEY
-const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-// AI 객체 생성
-const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
-
-
